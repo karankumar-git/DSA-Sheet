c52114f823c6af1d43a1ad0f7cb55ddc8fc34a2f
Karan Kumar <karankumarsonkaria@gmail.com>
2022-05-21 22:50:44 +0530

    Commiting code of power of xn with optimal

--- 14_powerofxn.cpp
+++ 14_powerofxn.cpp
@@ -11,10 +11,32 @@ double brute(double x,int n){
 	return ans;
 }
 
+double optimal(double x,int n){
+	double ans = 1.00;
+	bool isNegative = false;
+	if(n<0){
+		isNegative = true;
+		n = -1*n;
+	}
+	while(n>0){
+		if(n%2==0){
+			x *= x;
+			n /= 2;
+		}
+		else{
+			ans *= x;
+			n = n-1;
+		}
+	}
+	if(isNegative)	return double(1.00)/double(ans);
+	return ans;
+}
+
 int main(){
 	double x ;
 	cin>>x;
 	int n;
 	cin>>n;
 	cout<<brute(x,n)<<endl;
+	cout<<optimal(x,n)<<endl;
 }
